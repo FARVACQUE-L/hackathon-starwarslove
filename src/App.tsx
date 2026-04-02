46104541bd04ad58abd3aa695b575a90aae08aac
Solene Mendes <mendessolene@gmail.com>
2026-04-02 15:13:13 +0200

    Create Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import "./App.css";
 
-function App() {
-	const [count, setCount] = useState(0);
+import NavBar from "./components/NavBar";
 
-	return <></>;
+function App() {
+
+	return <>
+	<NavBar />
+	</>;
 }
 
 export default App;
