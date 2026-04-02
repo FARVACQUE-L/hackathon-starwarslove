4e8349c360598cf896af35af4c2a59db65da9313
AUDE CHARRIER <charrier.aude@gmail.com>
2026-04-02 20:09:55 +0200

    InputSearch ok

--- src/components/InputSearch.tsx
+++ src/components/InputSearch.tsx
@@ -3,43 +3,22 @@ import { useEffect } from "react";
 
 import "./InputSearch.css";
 
-//faire une props avec for pour label
-
-interface LabelProps {
-	for: "string";
-}
-
 function InputSearch() {
 	interface Character {
 		name: string;
 	}
-	const [characters, setCharacters] = useState<Character[]>([]);
+
+	const [characters, setCharacters] = useState<Character[]>([]); //typage tableau de plusieurs Character
 
 	useEffect(() => {
-		fetch("https://miadil.github.io/HarryPotterApi/api/json/characters.json")
+		fetch("https://miadil.github.io/starwars-api/api/all.json")
 			.then((res) => {
 				return res.json();
 			})
 			.then((data) => setCharacters(data));
 	}, []);
 
-	console.log(characters);
-
-	// input
-	const [inputValue, setInputValue] = useState("");
-
-	/* 	function filtrer(e: React.ChangeEvent<HTMLLiElement>) {
-        //renommer la fonction
-        pour affihcer les li qui correpsondent à ce qui est tapé
-        } */
-
-	function onChange(e: React.ChangeEvent<HTMLInputElement>) {
-		/* appeler Filtrer ; */
-		setInputValue(e.target.value);
-		setIsVisible(true);
-	}
-
-	//ul liste
+	//ul liste invisible, on la mettra visible à la frappe
 	const [isVisible, setIsVisible] = useState(false);
 
 	const listStyle: React.CSSProperties = {
@@ -47,46 +26,67 @@ function InputSearch() {
 		top: "100%",
 		left: "50%",
 		zIndex: 100,
-		display: isVisible ? "block" : "none", // ← conditionnel par rapport au usestate !
+		display: isVisible ? "block" : "none", // conditionnel par rapport au usestate !
 	};
+
+	// input
+	const [inputValue, setInputValue] = useState(""); //suffit pour typer en string
+
+	const [filteredCharacters, setFilteredCharacters] =
+		useState<Character[]>(characters);
+
+	function filterCharacters(value: string) {
+		setFilteredCharacters(
+			characters.filter(
+				(character) =>
+					character.name.toLowerCase().startsWith(value.toLowerCase()), // mise en minusucle pour comparer sinon.... !!
+			),
+		);
+	} // on filtre les characters (s'activera selon ce qu'on tape dans l'input)
+
+	/* autre solution plutot que fonction et usestate : 
+    const filteredCharacters = characters.filter((character) =>
+        character.name.startsWith(inputValue),
+    ); */
+
+	function onChange(e: React.ChangeEvent<HTMLInputElement>) {
+		setInputValue(e.target.value); // on actualise inputValue
+		filterCharacters(e.target.value); // on filtre
+		setIsVisible(true); // on affiche la liste
+	}
 
 	// li
 	function handleSelect(name: string) {
-		setInputValue(name);
+		setInputValue(name); //on actualise inputValue selon la ligne sélectionnée
 	}
 
 	return (
-		<>
-			<div className="relative">
-				<label for="character">Sélectionnez votre personnage : </label>
-				<input
-					id="character1"
-					name="character"
-					list="characters"
-					placeholder="Personnage 1"
-					value={inputValue}
-					onChange={(e) => onChange(e)}
-					onBlur={() => setIsVisible(false)}
-				></input>
-				<ul style={listStyle}>
-					{characters.map((character) => (
-						<li
-							key={character.name}
-							onMouseDown={() => handleSelect(character.name)}
-							onKeyDown={(e) =>
-								e.key === "Enter" && handleSelect(character.name)
-							}
-						>
-							{character.name}
-						</li>
-						//onMouseDown pour que ça soit joué à l'appui du bouton-souris, avant onBlur qui masque la liste
-						//onKeyDown : idem pour l'accessibilité
-						//onBlur: dès qu'on clique ailleurs
-					))}
-				</ul>
-			</div>
-			<figure></figure>
-		</>
+		//htmlFor à la place de for, parce qu'on est en JSX et for est un terme JS.... comme class --> className
+		<div className="relative">
+			<label htmlFor="character">Sélectionnez votre personnage : </label>
+			<input
+				id="character1"
+				name="character"
+				placeholder="Personnage 1"
+				value={inputValue}
+				onChange={(e) => onChange(e)}
+				onBlur={() => setIsVisible(false)}
+			></input>
+			<ul style={listStyle}>
+				{filteredCharacters.map((character) => (
+					<li
+						key={character.name}
+						onMouseDown={() => handleSelect(character.name)}
+						onKeyDown={(e) => e.key === "Enter" && handleSelect(character.name)}
+					>
+						{character.name}
+					</li>
+					//onMouseDown pour que ça soit joué à l'appui du bouton-souris, avant onBlur qui masque la liste
+					//onKeyDown : idem pour l'accessibilité
+					//onBlur: dès qu'on clique ailleurs
+				))}
+			</ul>
+		</div>
 	);
 }
 
